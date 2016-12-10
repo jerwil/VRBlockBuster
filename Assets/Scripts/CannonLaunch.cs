6a2bdee1dd9d21e87df4dc251e3a735fc999add6
Jeremy Wilson <jeremy@jeremyadamwilson.com>
2016-12-10 00:10:06 -0800

    Created simple scene with cannonball blasting simple structure

---
+++ Assets/Scripts/CannonLaunch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonLaunch : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.AddForce(0,0,thrust);
+
+    }
+}
